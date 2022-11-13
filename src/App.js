4f999b1b5db007c0b66e64985c41bf89ba74c0a2
Satya-ch45 <97754760+Satya-ch45@users.noreply.github.com>
2022-11-13 12:59:45 -0600

    Feedback Form Done deals in progress

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import SearchFlight from './Components/Flights/searchflight';
 import Flightdetails from './Components/Flights/flightdetails';
 import BookForm from './Components/Flights/bookform';
 import EditUser from './Users/EditUser';
+import Feedback from './Components/Feedback/Feedbackform';
 
 
-function App() {
+function App() { 
+ 
   return (
     <div className='App'>
       <Router>
@@ -22,12 +24,14 @@ function App() {
           <Route exact path="/Flightdetails" element={<Flightdetails />} />
           <Route exact path="/bookform" element={<BookForm />} />
           <Route exact path="/edituser/:id" element={<EditUser />} />
-          
+          <Route exact path="/feedbackform" element={<Feedback />} />
         </Routes>
 
       </Router>
-
+      
+      
     </div>
+    
   );
 }
 
