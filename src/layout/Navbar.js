b9311296fe24e8614a4c6097b04666e076991a50
Satya-ch45 <97754760+Satya-ch45@users.noreply.github.com>
2022-11-25 23:33:43 -0600

    Working master

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -2,9 +2,7 @@ import { RateReview } from '@mui/icons-material'
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Fab } from '@mui/material'
-import { Nav, NavDropdown, Container} from 'react-bootstrap';
-import { UserContext } from '../Users/UserContext';
-import { useContext } from 'react';
+import { Nav, NavDropdown } from 'react-bootstrap'
 
 const styles = {
     position: "fixed",
@@ -15,40 +13,36 @@ const styles = {
 export default function Navbar() {
 
     //getting logged in user from local storage
-    // let loggedinUser = JSON.parse( localStorage.getItem("user-info"));
-    // console.warn(loggedinUser);
-    const { loggedinUser, SetloggedInUser } = useContext(UserContext);
+    let loggedinUser = JSON.parse( localStorage.getItem("user-info"));
+    console.warn(loggedinUser);
+
     let navigate = useNavigate();
 
-    // const history = unstable_HistoryRouter();
+   // const history = unstable_HistoryRouter();
 
     //function for logout
-    function logOut() {
-        // localStorage.clear();
-        SetloggedInUser(null);
-        navigate("/searchFlight");
+    function logOut(){
+        localStorage.clear();
+        navigate("/flightsearch");
     }
 
     return (
         <div>
             <nav className="navbar navbar-expand-lg  navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <pre className="navbar-brand" >Travel-Easy</pre>
+                    <a className="navbar-brand" >Travel-Easy</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     {
-
-                        loggedinUser ?
+                       
+                        localStorage.getItem("user-info") ?
                             <>
-                                <Link className='btn btn-outline-light' to="/searchFlight">Flights
+                                <Link className='btn btn-outline-primary' to="/searchFlight">Flights
                                 </Link>
                                 <Link className='btn btn-outline-light' to="/hotels">Hotels
                                 </Link>
-                                <Link className='btn btn-outline-light' to="/flightstatus">Flight Status
-                                </Link>
-                                <Link className='btn btn-outline-light' to="/deals">Deals
                                 <Link className='btn btn-outline-light' to="/searchdeals">Deals
                                 </Link>
 
@@ -72,47 +66,28 @@ export default function Navbar() {
                     }
 
 
-
+                 
 
                     {
-                        //localStorage.getItem("user-info") ?
-                        loggedinUser ?
-                            <Nav>
-                                <NavDropdown title={loggedinUser && (JSON.parse(loggedinUser).firstName)}>
-                                    <NavDropdown.Item onClick={logOut}>
-                                        Logout
-                                    </NavDropdown.Item>
+                    localStorage.getItem("user-info") ?
+                     <Nav>
+                     <NavDropdown title = {loggedinUser && loggedinUser.firstName} className ='loggeduser'>
+                        <NavDropdown.Item onClick = {logOut}>
+                            Logout
+                        </NavDropdown.Item>
 
-                                </NavDropdown>
-                            </Nav>
-                            : null
-
-                    }
-
+                     </NavDropdown>
+                </Nav>
+                :null
+                    
+                }
+                   
 
                 </div>
             </nav>
 
-            <div id='bottom'>
-                <Link className='btn btn-outline-light' to="/feedbackform"><Fab sx={styles}><RateReview /></Fab></Link>
-            </div>
-
-            <>
-                {/* <Navbar>
-                    <Container>
-                        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-                        <Navbar.Toggle />
-                        <Navbar.Collapse className="justify-content-end">
-                            <Navbar.Text>
-                                Signed in as: <a href="#login">Mark Otto</a>
-                            </Navbar.Text>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar> */}
-            </>
            
         </div>
-
 
 
 
