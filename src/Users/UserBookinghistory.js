7aae4496ae57fd2dac9d7e72b877cb2363530303
Divyaja Kakarla <aoc38@txstate.edu>
2022-11-29 21:22:52 -0600

    BH

--- src/Users/UserBookinghistory.js
+++ src/Users/UserBookinghistory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 import { useEffect } from 'react';
 
 export default function UserBookinghistory() {
@@ -9,6 +10,14 @@ export default function UserBookinghistory() {
     console.log("logged in user in booking history", loggedinUser);
     let id = JSON.parse(loggedinUser).id;
     console.log("logged in user id", id);
+
+
+    const [userHistory, setUserHistory] = useState({
+        deals: [],
+        flights:[],
+        hotels:[]
+    });
+
     //on load of form
     useEffect(() => {
         loadUser();
@@ -16,9 +25,12 @@ export default function UserBookinghistory() {
     const loadUser = async () => {
         const result = await axios.get(`http://localhost:8080/userbookinghistory/${id}`);
         // setUser(result.data);
-        console.log(result);
+        console.log(result.data);
+        setUserHistory(result.data);
     };
     return (
-        <div>UserBookinghistory</div>
+        <div>
+            UserData
+        </div>
     )
 }
